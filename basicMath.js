127f35840964ba5d294a4f9c700e42e4a486f40c
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-01 15:10:02 +0600

    first problem with javascript

---
+++ basicMath.js
@@ -0,0 +1,14 @@
+//** Problem no 1
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+// solve:
+
+let myAge = 15;
+let humanDogRatio = 7;
+
+let myDogAge = myAge * humanDogRatio;
+
+console.log(myDogAge);

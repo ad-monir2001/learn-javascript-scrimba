f3e2d6629cf4d74f81d67f43e6b0c13bd29b52b3
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-01 15:24:23 +0600

    basic math have learned well

--- basicMath.js
+++ basicMath.js
@@ -12,3 +12,24 @@ let humanDogRatio = 7;
 let myDogAge = myAge * humanDogRatio;
 
 console.log(myDogAge);
+
+//** Problem no 2
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+
+
+// solve: 
+
+let bonusPoints = 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);

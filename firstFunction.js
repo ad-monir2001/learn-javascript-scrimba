4ba7d6bd276a4112836df49a77acad6f9ed57fa5
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-02 11:42:47 +0600

    function practice done

---
+++ firstFunction.js
@@ -0,0 +1,55 @@
+//**  PROBLEM
+
+// Create a function (you decide the name) that logs out the number 42 to the console
+// Call/invoke the function
+
+
+
+//** Solve:
+
+function gratings(){
+    console.log("Good Morning !!");
+}
+
+gratings();
+gratings();
+gratings();
+
+
+//**  PROBLEM
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+// Create a function that logs out the sum of all the lap times
+
+//** Solve:
+
+function sum(){
+    let totalTime = lap1 + lap2 + lap3 
+    console.log(totalTime)
+}
+
+sum()
+
+
+//**  PROBLEM
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+
+//** Solve:
+
+let lapsCompleted = 0
+
+function lap() {
+    lapsCompleted = lapsCompleted + 1
+
+    console.log(lapsCompleted)
+}
+
+lap()
+lap()
+lap()

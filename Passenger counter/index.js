a7a9140fab897fbd6db0e07d8fd45e5c334051e1
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-02 12:09:45 +0600

    some update on javascript

--- Passenger counter/index.js
+++ Passenger counter/index.js
@@ -1,3 +1,12 @@
-function increment(){
-    console.log("The button was clicked");
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    console.log(count)
 }
+
+increment()
+
+
+document.getElementById("count").innerText = 5

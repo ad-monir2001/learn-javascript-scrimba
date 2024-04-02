97adccd0401350796038619cb358bb8fe1ed8e02
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-02 18:01:55 +0600

    String variable learned

---
+++ stringVariable.js
@@ -0,0 +1,35 @@
+let message = "You have three new notifications";
+
+let userName = "Moniruzzaman Monir"
+
+console.log(userName + ", " + message + "!");
+
+//** PROBLEM 
+
+// Create a variable, messageToUser, that contains the message we have logged
+
+// solve:
+
+let messageToUser = "We have logged";
+
+console.log(messageToUser);
+
+
+//** PROBLEM 
+
+// Create two variables, name and greeting. The name variable should store your name,
+// and the greeting should store e.g. "Hi, my name is "
+
+// Create a third variable, myGreeting, that contatenates the two strings
+// Log myGreeting to the console
+
+
+// ! solve:
+
+let Name = "Moniruzzaman Monir";
+
+let greeting = "Hi, my name is "
+
+let myGreeting = greeting + Name;
+
+console.log(myGreeting)

3832fd27a719393cc09e1a80f7cb1655025525d0
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-03 13:52:32 +0600

    vairable practice done

---
+++ Practice part 1/variablePrac.js
@@ -0,0 +1,17 @@
+//** Problem  */
+
+// Create two variables, firstName and lastName
+
+// Concatenate the two variables into a third variable called fullName
+
+// Log fullName to the console
+
+// ! solve:
+
+let firstName = 'Moniruzzaman ';
+
+let lastName = 'Monir';
+
+let fullName = firstName + lastName;
+
+console.log(fullName);

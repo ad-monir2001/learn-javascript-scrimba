8749278fb5ba6e313724982ea30c822b1e066537
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-03 11:57:52 +0600

    Passenger counter app done

--- Passenger counter/index.js
+++ Passenger counter/index.js
@@ -1,14 +1,17 @@
-let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById('save-el');
+
+let countEl = document.getElementById('count-el');
 
 let count = 0;
 
 function increment() {
-  count = count + 1;
-  countEl.innerText = count;
+  count += 1;
+  countEl.textContent = count;
 }
-
 
 function save() {
-    console.log(count)
+  let countStr = count + ' - ';
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
 }
-

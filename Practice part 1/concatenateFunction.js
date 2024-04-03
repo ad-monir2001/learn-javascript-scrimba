c24ae2c1b8790a1119877361d09426982f50e72e
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-03 14:25:27 +0600

    concatenate function done

---
+++ Practice part 1/concatenateFunction.js
@@ -0,0 +1,16 @@
+let name = 'Linda ';
+let greeting = 'Hi there ';
+
+// ** Problem:
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+// ! Solve:
+
+function gratings() {
+  let gratingMessage = greeting + name;
+
+  console.log(gratingMessage);
+}
+
+gratings();
--- firstFunction.js
+++ firstFunction.js
@@ -3,53 +3,48 @@
 // Create a function (you decide the name) that logs out the number 42 to the console
 // Call/invoke the function
 
-
-
 //** Solve:
 
-function gratings(){
-    console.log("Good Morning !!");
+function gratings() {
+  console.log('Good Morning !!');
 }
 
 gratings();
 gratings();
 gratings();
 
-
 //**  PROBLEM
 
-let lap1 = 34
-let lap2 = 33
-let lap3 = 36
+let lap1 = 34;
+let lap2 = 33;
+let lap3 = 36;
 
 // Create a function that logs out the sum of all the lap times
 
 //** Solve:
 
-function sum(){
-    let totalTime = lap1 + lap2 + lap3 
-    console.log(totalTime)
+function sum() {
+  let totalTime = lap1 + lap2 + lap3;
+  console.log(totalTime);
 }
 
-sum()
-
+sum();
 
 //**  PROBLEM
 
 // Create a function that increments the lapsCompleted variable with one
 // Run it three times
 
-
 //** Solve:
 
-let lapsCompleted = 0
+let lapsCompleted = 0;
 
 function lap() {
-    lapsCompleted = lapsCompleted + 1
+  lapsCompleted += 1;
 
-    console.log(lapsCompleted)
+  console.log(lapsCompleted);
 }
 
-lap()
-lap()
-lap()
+lap();
+lap();
+lap();

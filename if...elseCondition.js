96a32b9630884e4ff49a3aa1794e8166ee4977ed
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-04 08:27:38 +0600

    if else condition added to learn

---
+++ if...elseCondition.js
@@ -0,0 +1,27 @@
+// let age = 18;
+
+// if (age >= 18 ){
+//     console.log("You are allowed to go!!")
+// } else{
+//     console.log("You are not allowed.")
+// }
+
+// ! Problem
+
+// Check if the person is elegible for a birthday card from the King! (100)
+
+let age = -8;
+
+// if less than 100    -> "Not elegible"
+// else if exactly 100 -> "Here is your birthday card from the King!"
+// else                -> "Not elegible, you have already gotten one"
+
+// ** Solve:
+
+if (age < 100) {
+  console.log('Not elegible');
+} else if (age === 100) {
+  console.log('Here is your birthday card from the King!');
+} else {
+  console.log('Not eligeble , you have already gotten one');
+}

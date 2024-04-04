40879c020dc3a709cdbb15871b96a0b3c4fa42f2
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-04 12:02:21 +0600

    Function added to this project blackjack

---
+++ BlackJack/index.js
@@ -0,0 +1,19 @@
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = '';
+
+
+function startGame() {
+  if (sum <= 20) {
+    message = 'Do you want to draw a new card? 🙂';
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+  }
+}

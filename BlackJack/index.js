7b3f5cd753607ab1b21b1e2871c7358e70805140
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-04 12:25:07 +0600

    all card and sum message done

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -5,10 +5,11 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = '';
 let messageEl = document.getElementById('message-el');
-// let sumEl = document.getElementById('sum-el');
 let sumEl = document.querySelector('#sum-el');
+let cardsEl = document.getElementById('cards-el');
 
 function startGame() {
+  cardsEl.textContent = 'Cards: ' + firstCard + ' ' + secondCard;
   sumEl.textContent = 'Sum: ' + sum;
 
   if (sum <= 20) {

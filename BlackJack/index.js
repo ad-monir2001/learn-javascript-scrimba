51411f26f7858ba6fc86348e183b506e88028986
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-04 12:19:16 +0600

    explore query selector

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,13 +1,16 @@
 let firstCard = 10;
-let secondCard = 1;
+let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = '';
-
 let messageEl = document.getElementById('message-el');
+// let sumEl = document.getElementById('sum-el');
+let sumEl = document.querySelector('#sum-el');
 
 function startGame() {
+  sumEl.textContent = 'Sum: ' + sum;
+
   if (sum <= 20) {
     message = 'Do you want to draw a new card? 🙂';
   } else if (sum === 21) {

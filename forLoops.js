1ebacfdfa488a9304722aeeae169ea216fbd21ae
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-07 11:20:56 +0600

    for loop extend more with array

--- forLoops.js
+++ forLoops.js
@@ -12,19 +12,30 @@
 
 
 
-let messages = [
-    "Hey, how's it going?",        
-    "I'm great, thank you! How about you?",
-    "All good. Been working on my portfolio lately.",
-    "Same here!",
-    "Great to hear",
-    "Great to hear",
-    "Great to listen",
-    "Great to see",
-    "Great to do",
-    "🙌"
-]
+// let messages = [
+//     "Hey, how's it going?",        
+//     "I'm great, thank you! How about you?",
+//     "All good. Been working on my portfolio lately.",
+//     "Same here!",
+//     "Great to hear",
+//     "Great to hear",
+//     "Great to listen",
+//     "Great to see",
+//     "Great to do",
+//     "🙌"
+// ]
 
-for (i = 0; i < messages.length; i += 1) {
-    console.log(messages[i])
+// for (i = 0; i < messages.length; i += 1) {
+//     console.log(messages[i])
+// }
+
+
+let cards = [7, 3, 9, 10, 15]
+
+// Create a for loop that logs out all the cards in the array
+// Use cards.length to specify how long the loop should run
+
+
+for ( i = 0; i < cards.length; i++) {
+    console.log(cards[i])
 }

fb24e53cfc2c88ff2ff71f9a62b0e6ce483701ea
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-07 10:32:37 +0600

    for loop and array combind

--- loops.js
+++ loops.js
@@ -1,3 +1,30 @@
-for (let count = 10; count < 21; count += 1) {
-  console.log(count);
+// for (let count = 10; count < 21; count += 1) {
+//   console.log(count);
+// }
+
+
+// Create a for loop that counts from 10 to 100 in steps of 10
+// Use console.log to log out the numbers
+
+// for (let i = 10; i <= 100; i += 10) {
+//     console.log(i)
+// } 
+
+
+
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately.",
+    "Same here!",
+    "Great to hear",
+    "Great to hear",
+    "Great to listen",
+    "Great to see",
+    "Great to do",
+    "🙌"
+]
+
+for (i = 0; i < messages.length; i += 1) {
+    console.log(messages[i])
 }

a8caa612e398295657cc58a951854d5cf0477f23
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-08 12:05:15 +0600

    solved logical and operator

---
+++ logicalOperator.js
@@ -0,0 +1,13 @@
+let hasSolvedChallenge = false;
+let hasHintsLeft = false;
+
+// Create an if statement that checks that both variables are false.
+// If so, run the showSolution() function
+
+if (hasSolvedChallenge === false && hasHintsLeft ===  false){
+    showSolution()
+}
+
+function showSolution() {
+    console.log('Showing the solution....');
+}

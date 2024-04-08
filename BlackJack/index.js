6e5e56f94fb38ac7b9c57015f1d548d317533562
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-08 15:12:03 +0600

    logical operator has used in blackjack game and operated well

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -49,8 +49,10 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = getRandomCard();
-  sum += card;
-  cards.push(card);
-  renderGame();
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }

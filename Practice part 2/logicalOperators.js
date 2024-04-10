c757b937b951776aced4f782616a7987b89c84b2
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-10 09:17:56 +0600

    logical operator problem solved

---
+++ Practice part 2/logicalOperators.js
@@ -0,0 +1,9 @@
+let dayOfMonth = 13;
+let weekday = 'Friday';
+
+// If it is Friday the 13th, log out this spooky face: 😱
+// Use the logical "AND operator" -> &&
+
+if (dayOfMonth === 13 && weekday === 'Friday') {
+  console.log('😱');
+}

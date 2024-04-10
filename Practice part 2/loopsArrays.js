1d24f02b24cc08f900a00a4a90d4141c5ebf2dee
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-10 08:52:21 +0600

    solved loops arrays problem

---
+++ Practice part 2/loopsArrays.js
@@ -0,0 +1,19 @@
+let largeCountries = ['China', 'India', 'USA', 'Indonesia', 'Pakistan'];
+
+// ! Problem:
+
+/* Use a for loop to log the following to the console:
+
+The 5 largest countries in the world:
+- China
+- India
+- United States
+- Indinesia
+- Pakistan
+*/
+
+// ** Solve:
+
+for (i = 0; i < largeCountries.length; i++) {
+  console.log('- ' + largeCountries[i]);
+}

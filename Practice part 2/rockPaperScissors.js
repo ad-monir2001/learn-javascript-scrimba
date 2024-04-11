3bd4ca44052635a323444274d7f4dcbccfbc0dca
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-11 06:31:23 +0600

    rock paper scissor game has been updated

--- Practice part 2/rockPaperScissors.js
+++ Practice part 2/rockPaperScissors.js
@@ -3,7 +3,7 @@ let hands = ['rock', 'paper', 'scissor'];
 // Create a function that returns a random item from the array
 
 function randomItems() {
-  let randomIndex = Math.floor( Math.random() * 3)
+  let randomIndex = Math.floor( Math.random() * hands.length)
   return hands[randomIndex]
 }
 

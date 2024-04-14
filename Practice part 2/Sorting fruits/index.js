f2a6e9f43b0a04d05e9979202f24b0578cba6465
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-14 08:11:04 +0600

    fruit short done

--- Practice part 2/Sorting fruits/index.js
+++ Practice part 2/Sorting fruits/index.js
@@ -5,3 +5,15 @@ let orangeShelf = document.getElementById('orange-shelf');
 // Create a function that puts the apples onto the appleShelf
 // and the oranges onto the orangeShelf. Use a for loop,
 // a conditional statement, and the textContent property.
+
+function fruitSort() {
+  for (i = 0; i < fruit.length; i++) {
+    if (fruit[i] === '🍎') {
+      appleShelf.textContent += '🍎';
+    } else if (fruit[i] === '🍊') {
+      orangeShelf.textContent += '🍊';
+    }
+  }
+}
+
+fruitSort();

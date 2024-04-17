8e23d4c98684b1904fc46972d0db31520d221b3a
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-17 17:15:15 +0600

    const come to the shoot

--- BuildCrome Extension/index.js
+++ BuildCrome Extension/index.js
@@ -1,3 +1,9 @@
-function saveLead() {
-  console.log('the button was clicked');
-}
+let myLeads = [];
+
+const inputEl = document.getElementById('input-el');
+
+const Input = document.getElementById('input-btn');
+
+Input.addEventListener('click', function () {
+  console.log('The button was clicked');
+});

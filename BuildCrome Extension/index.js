fb2c43d10ee208f67cc3608e98f3c80b7677343e
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-27 09:47:35 +0600

    Updated on the chrome extension

--- BuildCrome Extension/index.js
+++ BuildCrome Extension/index.js
@@ -12,14 +12,13 @@ inputBtn.addEventListener('click', function () {
 function renderLeads() {
   let listItems = '';
   for (let i = 0; i < myLeads.length; i++) {
-    // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
     listItems += `
-          <li>
-              <a target='_blank' href='${myLeads[i]}'>
-                  ${myLeads[i]}
-              </a>
-          </li>
-      `;
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `;
   }
   ulEl.innerHTML = listItems;
 }

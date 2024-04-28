b0499d64e8bdc35b3063b0f2b009de7ee48f018f
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-28 09:30:19 +0600

    local storage done

--- BuildCrome Extension/index.js
+++ BuildCrome Extension/index.js
@@ -2,10 +2,18 @@ let myLeads = [];
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById('ul-el');
+let leadsFromLocalStorage;
+localStorage.clear()
+leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+
+console.log(leadsFromLocalStorage);
 
 inputBtn.addEventListener('click', function () {
   myLeads.push(inputEl.value);
   inputEl.value = '';
+
+  localStorage.setItem('myLeads', JSON.stringify(myLeads));
+
   renderLeads();
 });
 

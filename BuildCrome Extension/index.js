f514e89372391334b978de97da9a5acba662b2df
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-29 07:01:19 +0600

    Massive update on function parameters

--- BuildCrome Extension/index.js
+++ BuildCrome Extension/index.js
@@ -1,4 +1,5 @@
 let myLeads = [];
+let oldLeads = [];
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById('ul-el');
@@ -7,34 +8,32 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
 
-deleteBtn.addEventListener('dblclick', function () {
-  localStorage.clear();
-  myLeads = [];
-  renderLeads();
-});
-
-inputBtn.addEventListener('click', function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = '';
-
-  localStorage.setItem('myLeads', JSON.stringify(myLeads));
-
-  renderLeads();
-});
-
-function renderLeads() {
+function render(leads) {
   let listItems = '';
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `;
   }
   ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener('dblclick', function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+inputBtn.addEventListener('click', function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = '';
+  localStorage.setItem('myLeads', JSON.stringify(myLeads));
+  render(myLeads);
+});

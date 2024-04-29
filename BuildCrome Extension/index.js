34e414bdf9d2c747e406df26e917c2be93776f1e
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-29 18:06:57 +0600

    Chrome extension build done

--- BuildCrome Extension/index.js
+++ BuildCrome Extension/index.js
@@ -5,11 +5,20 @@ const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById('ul-el');
 const deleteBtn = document.getElementById('delete-btn');
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+const tabBtn = document.getElementById('tab-btn');
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
+
+tabBtn.addEventListener('click', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem('myLeads', JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
 function render(leads) {
   let listItems = '';

4213c923f73c2b68cd88e62315eefdd3924fee3b
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-29 06:52:49 +0600

    delete button added on this project and added a double click function.

--- BuildCrome Extension/index.js
+++ BuildCrome Extension/index.js
@@ -2,11 +2,19 @@ let myLeads = [];
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById('ul-el');
-let leadsFromLocalStorage;
-localStorage.clear()
-leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+const deleteBtn = document.getElementById('delete-btn');
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
 
-console.log(leadsFromLocalStorage);
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
+deleteBtn.addEventListener('dblclick', function () {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+});
 
 inputBtn.addEventListener('click', function () {
   myLeads.push(inputEl.value);

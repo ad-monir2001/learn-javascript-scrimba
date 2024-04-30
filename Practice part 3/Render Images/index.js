9c47e65f7572546194b0b0056b57841b9286a47d
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-30 18:18:10 +0600

    solved the challenge of render images

--- Practice part 3/Render Images/index.js
+++ Practice part 3/Render Images/index.js
@@ -3,4 +3,14 @@
 // .innerHTML to solve the challenge.
 //oneline
 
+let containerEl = document.getElementById('container');
+
 const imgs = ['images/hip1.jpg', 'images/hip2.jpg', 'images/hip3.jpg'];
+
+function render() {
+  for (let i = 0; i < imgs.length; i++) {
+    containerEl.innerHTML += `<img class='team-img' src= ${imgs[i]} />`;
+  }
+}
+
+render();

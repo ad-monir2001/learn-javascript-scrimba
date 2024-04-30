cb14f84f5b580c839bda3b7bc35ada72bfeda186
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-30 22:20:51 +0600

    Rounding Numbers problem solve done

---
+++ Practice part 3/Rounding Numbers/index.js
@@ -0,0 +1,8 @@
+// Challenge:
+// Round the price in the button down to two decimal places.
+// Don't know which method to use? Google it!
+
+const totalPrice = 420.69235632455
+const btn = document.getElementById("purchase-btn")
+btn.textContent = `Buy €${ totalPrice.toFixed(2) }`
+

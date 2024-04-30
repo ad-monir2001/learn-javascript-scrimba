71d502481d112eabc8277e762db1f1c5893093b0
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-30 08:24:35 +0600

    save to local Storage done

---
+++ Practice part 3/save to localStorage/index.js
@@ -0,0 +1,8 @@
+// Save a value to localStorage
+localStorage.setItem('name', 'Monir');
+// Delete your code and refresh the page
+
+// Fetch your value from localStorage and log it out
+let x = localStorage.getItem('name');
+
+console.log(x);

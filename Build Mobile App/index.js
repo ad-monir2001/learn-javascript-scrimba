8eb48ec163b17f857a5e0740e4191d37d4a13ebe
Moniruzzaman <ad.monir2001@gmail.com>
2024-05-01 07:25:38 +0600

    Style of this project done

--- Build Mobile App/index.js
+++ Build Mobile App/index.js
@@ -1,4 +1,8 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+const inputFieldEl = document.getElementById('input-field');
+const addButtonEl = document.getElementById('add-button');
+
+addButtonEl.addEventListener('click', function () {
+  let inputValue = inputFieldEl.value;
+
+  console.log(inputValue);
+});
